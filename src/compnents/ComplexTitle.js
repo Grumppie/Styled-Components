767c621eddc6a3a3bc4e3a5b6967c7e009821596
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-05-06 15:32:30 +0530

    global variables

--- src/compnents/ComplexTitle.js
+++ src/compnents/ComplexTitle.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
+import { colors } from '../utils'
 
 const Wrapper = styled.div`
     h1{ 
         text-transform: uppercase;
         text-align: center;
+        color: ${colors.primary};
     }
     .underline{
         width: 30%;
         height: 5px;
-        background-color: #645cff;
+        background-color: var(--primary);
         margin: 0 auto;
     }
 `

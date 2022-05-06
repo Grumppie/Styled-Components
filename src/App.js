5c4be4f310af0dab0610f878d11362186c56e596
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-05-06 16:28:52 +0530

    styled card and nesting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import BasicTitle from "./compnents/BasicTitle"
 // import { BasicButton, HipsterButton } from "./compnents/Buttons"
-import ComplexTitle from "./compnents/ComplexTitle";
+// import ComplexTitle from "./compnents/ComplexTitle";
+
+import Card from "./compnents/Card";
 
 function App() {
   return (
     <div className="App">
-      <ComplexTitle title="more complex title" />
+      <Card />
     </div>
   );
 }
---
+++ src/compnents/Card.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Card = () => {
+    return (
+        <StyledCard>
+            <img src="https://dl.airtable.com/.attachmentThumbnails/e8bc3791196535af65f40e36993b9e1f/438bd160?ts=1651832554&userId=usrQMwWEPx18KgLcP&cs=d5ee4a9948c5c3b0" alt="product" />
+            <footer>
+                <h4>Product Name</h4>
+                <p>$15</p>
+            </footer>
+        </StyledCard>
+    )
+}
+
+const StyledCard = styled.article`
+    margin: 2rem;
+    width: 90vh;
+    max-width: 300px;
+    background-color: var(--white); 
+    border-radius: 0.5rem;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+    img{
+        width:100%;
+        border-top-left-radius: 0.25rem;
+        border-top-right-radius: 0.25rem;
+    }
+    footer{
+        display:flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 2rem;
+        h4{
+            color: var(--primary);
+            transition: all 0.3s ease-in-out;
+            /* &::before{
+                content: 'Name : ';
+                font-weight: lighter;
+            } */
+        }
+        p{
+            color: var(--primary);
+            font-weight: bold;
+            transition: all 0.3s ease-in-out;
+        }
+        &:hover{
+            h4,p{
+                color: var(--secondary);
+            }
+        }
+    }
+    &:hover{
+        box-shadow: 0px 0px 20px var(--hover-shadow);
+    }
+    @media (min-width: 768px){
+        max-width: 600px;
+    }
+
+`
+
+export default Card
